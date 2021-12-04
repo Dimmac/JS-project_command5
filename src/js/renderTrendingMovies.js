dbd3796a39c15b47628defffe5576c41ffe44ddb
Yuliia <84233109+Yuliia-dev@users.noreply.github.com>
2021-12-04 18:56:07 +0200

    check query result

--- src/js/renderTrendingMovies.js
+++ src/js/renderTrendingMovies.js
@@ -7,6 +7,7 @@ const galleryEl = document.querySelector('.container-movie');
 NewApiService.resetPage();
 NewApiService.fetchTrendingMovies().then(results => console.log(results));
 NewApiService.fetchGenre().then(results => console.log(results));
-NewApiService.fetchMovieForQuery().then(results => console.log(results));
-
-galleryEl.insertAdjacentHTML = '';
+//uncomment when will be in work
+//need pass NewApiService.query;
+// NewApiService.fetchMovieForQuery().then(results => console.log(results));
+NewApiService.galleryEl.insertAdjacentHTML = '';
