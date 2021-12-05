013803aa58ae221e3bd740f88794cd42c3e1e44d
Yuliia <84233109+Yuliia-dev@users.noreply.github.com>
2021-12-05 16:25:08 +0200

    add render main page

--- src/js/renderTrendingMovies.js
+++ src/js/renderTrendingMovies.js
@@ -1,15 +1,20 @@
 import ApiService from './api-service.js';
+import filmGallery from '../templates/film-card.hbs';
 
 const NewApiService = new ApiService();
 
 const galleryEl = document.querySelector('.container-movie');
 
-NewApiService.resetPage();
-
-NewApiService.fetchTrendingMovies().then(results => console.log(results));
-NewApiService.fetchGenre().then(results => console.log(results));
-NewApiService.fetchMovieById(2).then(results => console.log(results));
+// NewApiService.fetchTrendingMovies().then(renderGalleryTrendingMovie);
+// NewApiService.fetchGenre().then(results => console.log(results));
+// NewApiService.fetchMovieById(2).then(results => console.log(results));
 //uncomment when will be in work
 //need pass NewApiService.query;
 // NewApiService.fetchMovieForQuery().then(results => console.log(results));
-// NewApiService.galleryEl.insertAdjacentHTML = '';
+
+function renderGalleryTrendingMovie(data) {
+  const markup = filmGallery(data);
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+}
+
+NewApiService.fetchTrendingMovies().then(renderGalleryTrendingMovie);
