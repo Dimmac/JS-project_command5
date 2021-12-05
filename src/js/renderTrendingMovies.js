2340da2d100bbf70916d699e964d5278378f8085
Yuliia <84233109+Yuliia-dev@users.noreply.github.com>
2021-12-05 16:00:03 +0200

    add async/await, add metod fetchMovieById

--- src/js/renderTrendingMovies.js
+++ src/js/renderTrendingMovies.js
@@ -5,8 +5,10 @@ const NewApiService = new ApiService();
 const galleryEl = document.querySelector('.container-movie');
 
 NewApiService.resetPage();
+
 NewApiService.fetchTrendingMovies().then(results => console.log(results));
 NewApiService.fetchGenre().then(results => console.log(results));
+NewApiService.fetchMovieById(2).then(results => console.log(results));
 //uncomment when will be in work
 //need pass NewApiService.query;
 // NewApiService.fetchMovieForQuery().then(results => console.log(results));
