2e9974dad923d272639be5f4a23a2dec612f2573
MaryaSamsonenko <84477591+MaryaSamsonenko@users.noreply.github.com>
2021-12-07 22:36:57 +0200

    add pagination for search

--- src/js/renderMovieForQuery.js
+++ src/js/renderMovieForQuery.js
@@ -3,9 +3,9 @@ import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.2.min.css';
 import filmGallery from '../templates/film-card.hbs';
 import { formatData } from './formatted-data';
-import { renderGalleryTrendingMovie } from './renderTrendingMovies';
+import { pagination } from './renderTrendingMovies';
 
-const newApiService = new ApiService();
+export const searchQueryApiService = new ApiService();
 export const galleryEl = document.querySelector('.film__list');
 export const formEL = document.querySelector('.search-form');
 
@@ -13,29 +13,33 @@ formEL.addEventListener('submit', renderGalleryMovieForQuery);
 
 function renderGalleryMovieForQuery(e) {
   e.preventDefault();
-  try {
-    newApiService.query = e.currentTarget.elements.searchQuery.value.trim();
-    newApiService.fetchMovieForQuery(newApiService.query).then(showMovie).catch(console.log);
 
-    if (newApiService.query === '') {
-      Notiflix.Notify.failure('Sorry, there are movie not finding. Please try again.');
-      return;
-    }
+  searchQueryApiService.query = e.currentTarget.elements.searchQuery.value.trim();
+  console.log(searchQueryApiService.query);
+  searchQueryApiService
+    .fetchMovieForQuery(searchQueryApiService.query)
+    .then(data => {
+      pagination.reset();
+      ApiService.searchType = 'search';
+      searchQueryApiService.pageNum = 1;
+      showMovie(data);
+    })
+    .catch(console.log);
 
-    formEL.reset();
-  } catch (error) {
-    console.log(error);
+  if (searchQueryApiService.query === '') {
+    Notiflix.Notify.failure('Sorry, there are movie not finding. Please try again.');
+    return;
   }
 }
+formEL.reset();
 
 function showMovie(data) {
-  const formattedData = formatData(data.results);
-  const markup = filmGallery(formattedData);
-  galleryEl.innerHTML = markup;
   if (data.results.length < 1) {
-    renderGalleryTrendingMovie();
     Notiflix.Notify.failure('Sorry, there are movie not finding. Please try again.');
     formEL.reset();
     return;
   }
+  const formattedData = formatData(data.results);
+  const markup = filmGallery(formattedData);
+  galleryEl.innerHTML = markup;
 }
