f77112c41a952832ff6d823fc37e366df94fd610
Ievgeniia Vashchenko <dn050493vem@gmail.com>
2021-12-08 23:02:07 +0200

    исправляет ключ main и переносит ключи темы в свой файл

--- src/js/keys-local-storage.js
+++ src/js/keys-local-storage.js
@@ -4,3 +4,7 @@ export const STORAGE_KEY_MAIN = 'main';
 export const STORAGE_KEY_WATCHED = 'watched';
 export const STORAGE_KEY_QUEUE = 'queue';
 export const STORAGE_KEY_OPEN_MODAL = 'open-modal';
+export const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
