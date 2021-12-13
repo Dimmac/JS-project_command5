104d340d136c8abe11de6ee70d8d682545dd5d2c
Yuliia <84233109+Yuliia-dev@users.noreply.github.com>
2021-12-13 15:31:01 +0200

    тушим консоль

--- src/js/loader.js
+++ src/js/loader.js
@@ -2,7 +2,6 @@ const mask = document.querySelector('.mask');
 const link = document.querySelectorAll('a');
 
 window.addEventListener('load', onLoad);
-link.addEventListener('click', onLoad);
 
 export function onLoad() {
   mask.classList.add('hide');
