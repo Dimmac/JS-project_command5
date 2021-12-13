cfed9370ff18dfc0a59893ace4926556db87c585
MaryaSamsonenko <84477591+MaryaSamsonenko@users.noreply.github.com>
2021-12-13 13:22:03 +0000

    Deploying to gh-pages from @ Dimmac/JS-project_command5@fd6010d3564e22089616333dcbf54efe87188888 🚀

--- src.812d9e39.js
+++ src.812d9e39.js
@@ -112,7 +112,7 @@ var t;!function(e,n){"object"==typeof exports&&"object"==typeof module?module.ex
 },{"./renderTrendingMovies":"hOC4","../templates/film-card.hbs":"fFoL","./renderMovieForQuery":"cQZk","./formatted-data":"VDXL","notiflix":"hWOm","notiflix/dist/notiflix-3.2.2.min.css":"yBio","./saveTrendingTolocalStorage":"bh8W","./keys-local-storage":"QVCi","./api-service.js":"hC31","sweetalert":"ZQjc","animate.css":"yBio","nprogress":"Mkzz","nprogress/nprogress.css":"yBio"}],"JyJ9":[function(require,module,exports) {
 "use strict";var e=require("./keys-local-storage");const t=document.querySelector("#theme-switch-toggle"),s=document.querySelector(".footer");function c(){"dark-theme"===localStorage.theme?(document.body.classList.add(e.Theme.DARK),s.classList.add(e.Theme.DARK),t.checked=!0):(document.body.classList.add(e.Theme.LIGHT),s.classList.add(e.Theme.LIGHT))}function o(t){document.body.classList.toggle(e.Theme.LIGHT),document.body.classList.toggle(e.Theme.DARK),s.classList.toggle(e.Theme.LIGHT),s.classList.toggle(e.Theme.DARK),t.target.checked?localStorage.setItem("theme",e.Theme.DARK):localStorage.setItem("theme",e.Theme.LIGHT)}t.addEventListener("change",o),c();
 },{"./keys-local-storage":"QVCi"}],"z1HG":[function(require,module,exports) {
-const e=document.querySelector(".to-top");window.addEventListener("scroll",()=>{window.pageYOffset>600?e.classList.add("active"):e.classList.remove("active")});
+const e=document.querySelector(".to-top");function o(){window.scrollTo({top:0,behavior:"smooth"})}window.addEventListener("scroll",()=>{window.pageYOffset>600?e.classList.add("active"):e.classList.remove("active")}),e.addEventListener("click",o);
 },{}],"cPrW":[function(require,module,exports) {
 module.exports="/JS-project_command5/Dmytro-dev.7c6f085e.jpg";
 },{}],"cOi6":[function(require,module,exports) {
@@ -138,4 +138,4 @@ module.exports="/JS-project_command5/sprite.ae706612.svg";
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/get-genres"),require("./js/renderTrendingMovies.js"),require("./js/renderMovieForQuery.js"),require("./js/pagination");var e=require("./js/header-logic");require("./js/modal-card"),require("./js/thems"),require("./js/toTop"),require("./js/modal-team"),require("./js/loader");
 },{"./sass/main.scss":"clu1","./js/get-genres":"RCw5","./js/renderTrendingMovies.js":"hOC4","./js/renderMovieForQuery.js":"cQZk","./js/pagination":"o8lo","./js/header-logic":"h3xw","./js/modal-card":"DnHo","./js/thems":"JyJ9","./js/toTop":"z1HG","./js/modal-team":"O9Th","./js/loader":"vQE2"}]},{},["Focm"], null)
-//# sourceMappingURL=/JS-project_command5/src.3e35731e.js.map
+//# sourceMappingURL=/JS-project_command5/src.812d9e39.js.map
